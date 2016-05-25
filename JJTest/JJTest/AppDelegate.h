cdafc26a2e848958dc5bd7a1f68c948c75f3f0aa
jj0769 <jj0769@vip.qq.com>
2016-05-25 15:50:53 +0800

    新建了一个项目

---
+++ JJTest/JJTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JJTest
+//
+//  Created by lanou on 16/5/25.
+//  Copyright © 2016年 邓健豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
